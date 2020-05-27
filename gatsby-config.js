bff83a8fb1c658f754b7142b5bbaf9cf1b6d3f95
Alexander Moras <hello@alexmoras.com>
2020-05-27 09:17:16 +0100

    Basic site added, will need more updates but something was needed to fill the empty void that is my website.

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Alex Moras - Old school hacker and developer.`,
   },
   plugins: [
     {
@@ -24,9 +24,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Alex Moras`,
+        short_name: `Alex Moras`,
+        description: `Old school hacker and developer.`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
